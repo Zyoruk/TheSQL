968f41bc827100aaa3893e89ed9bdc76343b1023
Daniel Jenkins <jeukel@gmail.com>
2015-09-15 11:49:50 -0600

    DataCatalog and Tests class

---
+++ src/DataCatalog.py
@@ -0,0 +1,136 @@
+import json
+from os.path import abspath, dirname, join, isfile
+import os.path
+from os import listdir
+
+WHERE_AM_I = abspath(dirname(__file__))
+
+class DataCatalog(object):
+    
+    def __init__(self):
+        self.db = 0
+        self.sysCat = 0
+                
+    """READ"""
+    
+    def openSysCat(self,table):
+        with open(table + '.json', 'r') as self.sysCat:
+            self.db = json.load(self.sysCat)
+            
+    def hasIndex(self, table):  
+        self.openSysCat()
+        cat = self.db['index']
+        self.db.close()       
+        return cat
+            
+    def getType(self, table, column):
+        self.openSysCat(table)
+        num = 0                
+        for columnsParam in self.db["columns"]:
+            if columnsParam["name"] != column:
+                num += 1
+            else:
+                break   
+        cat = self.db["columns"][num]["type"]
+        self.sysCat.close()
+        return cat
+            
+    def getsPK(self, table):
+        self.openSysCat(table)
+        cat = self.db["PK"]
+        self.sysCat.close()
+        return cat
+    
+    def getColNames(self, table): 
+        self.openSysCat(table)
+        cat = []
+        num = 0
+        for columnsParam in self.db["columns"]:
+            cat.append(columnsParam["name"])
+            num += 1
+        self.sysCat.close()
+        return cat
+    
+    def getTabNames(self): #TODO check all json files in dir
+        onlyfiles = [ f for f in listdir(WHERE_AM_I) if isfile(join(WHERE_AM_I,f)) ]
+        tables = []
+        for jason in onlyfiles:            
+            if jason.endswith('.json'):
+                tables.append(jason)
+        tabs = []
+        for tab in tables:
+            tabs.append(tab.split(".",1)[0])
+        
+        return tabs
+                 
+    def getTypes(self, table):
+        self.openSysCat(table)
+        cat = []
+        num = 0
+        for columnsParam in self.db["columns"]:
+            cat.append(columnsParam["type"])
+            num += 1
+        self.sysCat.close()
+        return cat
+    
+    def getIndex(self, table, column):
+        self.openSysCat(table)
+        num = 0                
+        for columnsParam in self.db["columns"]:
+            if columnsParam["name"] != column:
+                num += 1
+            else:
+                break   
+        self.sysCat.close()
+        return num
+                
+    """WRITE"""
+    
+    def setNewDB(self):
+        with open('sysCat.json', 'w') as outfile:
+            json.dump({'Index':'false'}, outfile)
+          
+                
+    def setNewTable(self, table_name, columns_names, column_type, column_nullability, PK):
+        
+        tName = table_name + '.json'
+        
+        """Check for PK in columns"""
+        if PK in columns_names:        
+            """Not enough names for types"""
+            if len(columns_names) != len(column_type) or len(columns_names) != len(column_nullability):
+                #send error
+                return
+            else:            
+                
+                cols = []                           
+                while len(columns_names) != 0:                
+                    cols.append({'name':columns_names.pop(-1),'type':column_type.pop(-1),
+                                 'null':column_nullability.pop(-1)})
+                
+                table = {'PK': PK,
+                         'index':'false',
+                         'enabled':'true',
+                         'columns':cols}
+                                
+                if os.path.isfile(tName):
+                    #TO DO: Return error
+                    print('error')
+                else:
+                    with open(tName, 'w') as sysCat:
+                        json.dump(table,sysCat)
+        return
+
+            
+    def dropTable(self, table_name):
+        table = table_name + '.json'
+        with open(table , 'r') as sysCat:
+            db = json.load(sysCat)
+        sysCat.close()
+                
+        db["enabled"] = "false"
+        
+        with open(table, 'w') as sysCat:
+            json.dump(db,sysCat)
+        
+        return
---
+++ src/TestClass.py
@@ -0,0 +1,41 @@
+from DataCatalog import DataCatalog
+
+class TestClass(object):
+    
+    def __init__(self):
+        self.data = DataCatalog()
+    
+    def test1(self):
+                   
+        names = ['julia','maria','demonios']
+        types = ['int', 'double', 'char']
+        isNull = ['not null', 'null', 'null']    
+        
+        names1 = ['julia','maria','demonios']
+        types1 = ['int', 'double', 'char']
+        isNull1 = ['not null', 'null', 'null']
+        
+        self.data.setNewTable('mugre', names, types, isNull , 'julia')
+        self.data.setNewTable('moo', names1, types1, isNull1 , 'julia')
+        
+    def test2(self):
+        self.data.dropTable("mugre")
+    
+    def test3(self):
+        print(self.data.getType("mugre","demonios"))
+        print(self.data.getsPK("mugre"))
+        
+    def test4(self):
+        print(self.data.getColNames("mugre"))
+        print(self.data.getTypes("mugre"))
+        print(self.data.getTabNames())
+        print(self.data.getIndex("mugre", "julia"))
+
+
+if __name__ == '__main__':
+    print("This is 'Bases de Datos'!")
+    tests = TestClass()
+    tests.test4()
+    
+    
+    
