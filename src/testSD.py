270f457e66f2a0f2a21a2d6aea37423dc6b4ae91
zyoruk <luisangel1993julio@gmail.com>
2015-09-17 00:43:16 -0600

    Se prueban casos con arreglos, strings y bytes

--- src/testSD.py
+++ src/testSD.py
@@ -20,6 +20,7 @@ Created on Sep 15, 2015
 '''
 
 import storedData as SD
+from struct import  *
 class TestSDClass(object):
     '''
     classdocs
@@ -33,12 +34,61 @@ class TestSDClass(object):
         self.sd = None
         
     def create(self):
-        self.sd = SD.StoredData(20, 'test.json') #Crea una tabla
+        self.sd = SD.StoredData(20, 'test.json')
+    
+    def clear(self):
+        self.sd.erase()
+        if len(self.sd.keys()) != 0:
+            for key in self.sd.keys():
+                self.sd.remove(key)
+        self.sd.dump()
         
-    def randomInsert(self):
-        for i in range (0 , 10 ):            
+    def randomInsert(self, a = 1):
+        for i in range (0 , 10*a ):            
             self.sd.insert(i, [101010101])
+        self.sd.dump()
         
     def InsertDupl(self):
         self.sd.insert(5, [101010101])
         
+    def showAll(self):
+        return self.sd.getAll()
+    
+    def convertToBinary(self):
+        for item in self.sd.getAll():
+            print pack('iiii',1.23,2.23,3.23,4.23)
+            
+    
+    def binaryFiles(self):
+        fh = open('binarytest', 'w+b')
+        a = 5
+        s = chr(5)
+        fh.write(s.encode())
+        
+        fh.close()
+        fh = open('binarytest','rb')
+        print fh.readline()
+        
+    def insertBinary(self):
+        
+        self.sd.insert(0, pack('3sii','Rey', 5,10))
+        self.sd.insert(1, pack('3sii','Rey', 5,10))
+        self.sd.insert(2, pack('3sii','Rey', 5,10))
+        return self.sd.dump()
+        
+    def readall(self):
+        result = []
+        for item in  self.sd.getAll():
+            t = []
+            t.append(item[0])
+            print unpack('3sii', item[1])
+                
+        print result
+                          
+if __name__ == '__main__':
+    a = '234324324.2323'
+    
+    print '2' in a
+    
+    
+    
